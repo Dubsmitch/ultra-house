b0a8d5514a5df56f399652db493016e5de8f0aa3
wmitchell-bimaire <wmitchell@bimaire.com>
2022-05-29 16:07:23 -0600

    added a selected Day article that displays events for the day

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -1,68 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import styles from './Events.module.css';
 import './reactCalendar.css';
-import {eventDates} from './EventsObject';
+import { eventDates } from './EventsObject';
 import moment from 'moment';
+import { eventDatesInterface } from '../../types/types';
 
 
 interface EventsProps {
   props: any
 }
 
+interface eventObj {
+  selectedDayText?: string,
+  events?: eventDatesInterface,
+}
+
 const Events: React.FC<EventsProps> = ({props}) => {
 
+  const [selectedDay, setSelectedDay] = useState<eventObj>({})
+
   const handleDayClickCal = (value: any) => {
-    console.log('clicked', value);
+
+    const selectedDayText = moment(value).format("MMM Do YY");
+    console.log('selected thing', eventDates[selectedDayText]);
+    
+    type eventObjType = {
+      [key: string]: {
+        [key:string]: string
+      }
+    }
+
+    const eventObj: eventObjType = {};
+    eventObj[selectedDayText] = eventDates[selectedDayText];
+
+    const selectedDayObj = {
+      selectedDayText,
+      events: eventObj
+    }
+
+    setSelectedDay(selectedDayObj);
   }
 
   const onTileClick = (calendarDate: any, view: any) => {
 
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'];
+    // may use these constants later depending on needs
+    // const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+    //   'July', 'August', 'September', 'October', 'November', 'December'];
+    // let currentDate = null;
+    // let selectedDayDigits = null;
+
     let selectedDayString = null;
     let calendarDayString = null;
-    let currentDate = null;
-    let selectedDayDigits = null;
-
-
-
-    // console.log('using moment', moment().format("MMM Do YY"));  
-    // console.log('date to moment', moment("May 11 2022").format());
-
+    
     let calendarDay: number | Date | string = moment(calendarDate).format("MMM Do YY");
-    console.log('calendarDay', calendarDay);
-    // newDate = newDate.getTime();
 
     let thumbnail: string = '';
-    let description: string= '';
 
     if (eventDates[calendarDay]) {
       console.log(JSON.stringify(eventDates[calendarDay]));
       thumbnail = Object.keys(eventDates[calendarDay])[0];
-      description = eventDates[calendarDay][thumbnail];
-      console.log('thumbnail', thumbnail);
-      console.log('description', eventDates[calendarDay][thumbnail]);
     }
 
     if (view === 'month' && selectedDayString === calendarDayString) {
       return (
         <div>
-          {description}
+          {thumbnail}
         </div>
       );
     }
     return null;
   };
 
+  const renderEvents = () => {
+     
+    if (selectedDay.events) {
+      const selection = selectedDay.selectedDayText;
+      return Object.keys(selectedDay.events[selectedDay.selectedDayText]).map((key) => {
+        console.log(key);
+        return (
+          <li>
+            <p>
+              {key}: {selectedDay.events[selection][key]}
+            </p>
+          </li>
+        )
+      })
+    }
+    else {
+      return null
+    }
+  }
+
   return (
     <div className={styles.Events}>
       <Calendar
         onClickDay={(value) => handleDayClickCal(value)}
-
         tileContent={({ date, view }) => onTileClick(date, view)}
       />
+        <article className={styles.selectedEventsContainer}>
+          <span> Selected Day: </span>
+          <ul>
+            {renderEvents()}
+          </ul>
+        </article>
     </div>
   );
 }
--- src/components/events/EventsObject.tsx
+++ src/components/events/EventsObject.tsx
@@ -6,6 +6,10 @@ export const eventDates: eventDatesInterface = {
   },
   "May 8th 22": {
     "Pilot Peak Death March": "10-30 miles, somewhere in the SouthWest USA",
+    "Pilot Peak Death": "10-30 miles, somewhere in the SouthWest USA",
+    "Pilot Peak": "10-30 miles, somewhere in the SouthWest USA",
+    "Pilot": "10-30 miles, somewhere in the SouthWest USA",
+
   },
   "May 11th 22": {
     "Long run": "10-30 miles, somewhere in the SouthWest USA",
