d3f4fd2f209f7ce672957a9c9e86930e93d16178
wmitchell-bimaire <wmitchell@bimaire.com>
2022-05-29 20:43:35 -0600

    added some start/stop logic and hard coded variables for size and speed to the visualizer

--- src/components/sortVisualizer/SortVisualizer.tsx
+++ src/components/sortVisualizer/SortVisualizer.tsx
@@ -1,41 +1,47 @@
 import React, {useEffect, useState} from 'react';
 import styles from './SortVisualizer.module.css';
+// size of array to be sorted
+const sortArraySize = 255
+//speed of sort
+const sortSpeed = 20
 
 const SortVisualizer: React.FC = () => {
   const [sortArray, setSortArray] = useState([]);
-  const [selectedPointers, setSelectedPointers] = useState([]);
   const [numComps, setNumComps] = useState(0);
   const [numSwaps, setNumSwaps] = useState(0);
-  const [j, setJ] = useState(1);
+  const [sortActive, setSortActive] = useState(false);
+  const [j, setJ] = useState(0);
   const [i, setI] = useState(0);
+  const [speed, setSpeed] = useState(sortSpeed);
 
   useEffect (() => {
-    // initialize array
     createArray()
   }, [])
 
   useEffect(() => {
-    sortMyArray();
-  }, [sortArray])
-
-  useEffect(() => {
-    console.log('did this run from useEffect?');
     let interval: NodeJS.Timer = null
     
+    if(sortActive) {
       interval = setInterval(() => {
         bubbleSort();
-      }, 100)
+      }, speed)
+    } else {
+      clearInterval(interval);
+    }
 
     return () => clearInterval(interval)
-  }, [sortArray, j, i])
+  }, [sortActive, sortArray, j, i])
 
   /**
    * will create an array of 20 random numbers with no duplicates
    */
   const createArray = () => {
     const storageArray: number[] = [];
+    setSortActive(false);
+    setJ(0);
+    setI(0);
 
-    for (let i = 1; i < 21; i++) {
+    for (let i = 1; i < (sortArraySize + 1); i++) {
       storageArray.push(i);
     }
 
@@ -55,9 +61,17 @@ const SortVisualizer: React.FC = () => {
     setSortArray([...unique]);
   }
 
+  const changeSpeed = (direction: number) => {
+    if (direction > 0) {
+      setSpeed(currentSpeed => currentSpeed / 2);
+    } else {
+      setSpeed(currentSpeed => currentSpeed * 2);
+    }
+  }
+
   const renderSortedArray = () => {
     return sortArray.map((number, idx) => {
-      const divHeight = 10 + (number * 15);
+      const divHeight = 10 + (number * 3);
       let doesMatch = idx === j || idx === j+1;
       return (
         <div style={
@@ -71,7 +85,7 @@ const SortVisualizer: React.FC = () => {
           {
             height: `${divHeight}px`,
             width: '5px',
-            backgroundColor: 'rgb(255, 79, 4)',
+            backgroundColor: `rgb(${255-number}, 70,  ${0+number})`,
             marginLeft: '2px'
           }
         }>
@@ -81,39 +95,49 @@ const SortVisualizer: React.FC = () => {
     })
   }
 
-  const sortMyArray = () => {
-    setSelectedPointers([0, 1]);
+  const restartSort = () => {
+    setJ(0);
+    setI(0);
+    setSortActive(true);
   }
 
   const bubbleSort = () => {
     let dataArray = [...sortArray];
     const size = sortArray.length;
     let swap = dataArray[j];
-
-      if(j === size-i-1) {
-          setJ(0); 
-          setI(i+1);
-      }
+    
+    if(j === size-i-1) {
+      setJ(0); 
+      setI(i+1);
+    }
+    
+    if(i<size-1 && j<size-i-1) {
+      setNumComps(currentNum => currentNum + 1);
       
-      if(i<size-1 && j<size-i-1) {
-        setNumComps(currentNum => currentNum + 1);
-          if(swap > dataArray[j+1]) {
-              dataArray[j]=dataArray[j+1];
-              dataArray[j+1] = swap;
-              setNumSwaps(currentNum => currentNum + 1);
-              setSortArray(dataArray);
-              setJ(j+1); 
-          } else {
-              setJ(j+1);
-          }
+      if(swap > dataArray[j+1]) {
+        dataArray[j]=dataArray[j+1];
+        dataArray[j+1] = swap;
+        setNumSwaps(currentNum => currentNum + 1);
+        setSortArray(dataArray);
+        setJ(j+1); 
+      } else {
+        setJ(j+1);
       }
+    }
+  }
+
+  const pause = () => {
+    setSortActive(currentSortActive => !currentSortActive)
   }
 
   return (
     <div className={styles.SortVisualizer}>
       <button onClick={() => {createArray()}}> Randomize </button>
-      <button onClick={() => {bubbleSort()}}> BubbleSortME </button>
-
+      <button disabled={!!i || !!j} onClick={() => {restartSort()}}> Start Sorting </button>
+      <button onClick={() => {bubbleSort()}}> increment by one </button>
+      <button disabled={!j && !i} onClick={() => {pause()}}> {sortActive ? 'Pause' : 'Unpause'} </button>
+      <button onClick={() => {changeSpeed(1)}}> increase sort Speed </button>
+      <button onClick={() => {changeSpeed(0)}}> decrease sort Speed </button>
 
       {renderSortedArray()}
       <p>num comps: {numComps}</p>
